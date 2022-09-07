ce8c8cebb6eadcddac7d9087d685a61a301dd1a0
HenryC-3 <2324375439@qq.com>
2022-09-08 00:12:37 +0800

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+## Info
+
+Vue 3 tab component demo with
+
+-   `<script setup>`
+-   [dynamic-components](https://vuejs.org/guide/essentials/component-basics.html#dynamic-components)
+-   special built-in attribute `is`
+
+## Recommended IDE Setup
+
+-   [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar)
---
+++ src/components/TabComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <button v-for="tab in tabs" :key="tab" @click="currentTab = tab.component">
+        {{ tab.name }}
+    </button>
+    <component :is="currentTab ? currentTab : defaultTab"></component>
+</template>
+
+<script setup>
+import { markRaw, ref } from "vue";
+import tabOne from "./TabOne.vue";
+import tabTwo from "./TabTwo.vue";
+
+const tabs = ref([
+    { name: "Tab 1", component: markRaw(tabOne) },
+    { name: "Tab 2", component: markRaw(tabTwo) },
+]);
+const currentTab = ref(null);
+const defaultTab = tabOne;
+</script>
+
+<style scoped></style>
